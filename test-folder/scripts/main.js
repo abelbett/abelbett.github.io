593471dba95853c79f7bb37c57706ccbb2e0ba28
abelbett <86533217+abelbett@users.noreply.github.com>
2021-06-27 04:53:41 +1000

    Add files via upload

---
+++ test-folder/scripts/main.js
@@ -0,0 +1,40 @@
+let myimage = document.querySelector('img');
+
+myimage.onclick = function() {
+    let mySrc = myimage.getAttribute('src');
+    if(mySrc === 'images/image1.jpg') {
+      myimage.setAttribute('src','images/image2.jpg');
+    } else {
+      myimage.setAttribute('src','images/image1.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+  }
+}
